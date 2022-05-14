7b9ed8f15ef6303be779148c9afd3468ba495c96
Henrique Melo <henrique.melo@oryon.solutions>
2022-05-14 17:47:47 +0100

    Version update

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
-import MongoAtlasDataApiSDK from './MongoAtlasDataApiSDK';
-import { BadRequest, NotFound, ServerError, Unauthorized } from './errors';
+import MongoAtlasDataApiSDK from './MongoAtlasDataApiSDK.js';
+import { BadRequest, NotFound, ServerError, Unauthorized } from './errors.js';
 export { BadRequest, Unauthorized, NotFound, ServerError };
 export default MongoAtlasDataApiSDK;
 //# sourceMappingURL=index.js.map
