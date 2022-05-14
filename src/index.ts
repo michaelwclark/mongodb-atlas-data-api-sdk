bb2dfefae5e66847cd13acc48ccc0177143cbdb7
Henrique Melo <henrique.melo@oryon.solutions>
2022-05-14 16:46:57 +0100

    Version update

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,6 @@
-import MongoAtlasDataApiSDK from './MongoAtlasDataApiSDK';
-import { BadRequest, NotFound, ServerError, Unauthorized } from './errors';
-import {
-  FindOneParams,
-  FindParams,
-  UpdateParams,
-  DeleteParams,
-  InsertManyParams,
-  InsertOneParams,
-  ReplaceOneParams,
-  AggregateParams} from './params';
+import MongoAtlasDataApiSDK from './MongoAtlasDataApiSDK'
+import { BadRequest, NotFound, ServerError, Unauthorized } from './errors'
+import { FindOneParams, FindParams, UpdateParams, DeleteParams, InsertManyParams, InsertOneParams, ReplaceOneParams, AggregateParams } from './params'
 
-export {
-  BadRequest,
-  Unauthorized,
-  NotFound,
-  ServerError,
-  FindOneParams,
-  FindParams,
-  UpdateParams,
-  DeleteParams,
-  InsertManyParams,
-  InsertOneParams,
-  ReplaceOneParams,
-  AggregateParams
-};
-export default MongoAtlasDataApiSDK;
+export { BadRequest, Unauthorized, NotFound, ServerError, FindOneParams, FindParams, UpdateParams, DeleteParams, InsertManyParams, InsertOneParams, ReplaceOneParams, AggregateParams }
+export default MongoAtlasDataApiSDK
